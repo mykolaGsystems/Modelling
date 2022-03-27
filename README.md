c433218ad22f31b936260219a8c713862dbf62f9
mykolaGsystems <kolangavrish0001@ukr.net>
2022-03-27 21:49:23 +0100

    Init

--- README.md
+++
@@ -1,2 +0,0 @@
-# ecolab3
-Another python implementation of Dawn Walker's ecolab (for Modelling and Simulation of Natural Systems: COM3001/6009)
--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from ecolab3.environment import Environment
-from ecolab3.agents import Fish, Pathogen
-from ecolab3 import run_ecolab, get_agent_counts, draw_animation
+from ecolab3.agents import Fish
+from ecolab3 import run_ecolab, get_agent_counts
 
-env = Environment(shape=[100,100],growrate=50,maxgrass=5,startgrass=1)
+env = Environment(shape=[100,100])
 
 agents = []
-#create agents (rabbits and foxes)
-Nfish = 40
+Nfish = 130
+Nfish_infected = 20
 
-NPathogen = 20
 for i in range(Nfish):
-    f = Fish(position=env.get_random_location(),speed=5)
+    f = Fish(position=env.get_random_location(),speed=2.5,infection_status=0)
     agents.append(f)
 
-for i in range(NPathogen):
-    f = Pathogen(position=env.get_random_location(),speed=0)
+for i in range(Nfish_infected):
+    f = Fish(position=env.get_random_location(),speed=2.5,infection_status=1)
     agents.append(f)
-
-record = run_ecolab(env,agents,Niterations=1000)
-# anim = draw_animation(fig,record[::5],5) #draw every 5th frame
-
+    
+record = run_ecolab(env,agents,Niterations=400)
 
 counts = get_agent_counts(record)
 plt.figure(figsize=[8,4])
 plt.plot(counts[:,0],label='fish')
+plt.plot(counts[:,1],label='pathogen')
 plt.legend()
 plt.grid()
 plt.xlabel('Iteration')
